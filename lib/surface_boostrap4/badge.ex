f6a121189a681281f9183b2970ce544b18de9913
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-06-16 07:11:26 +0200

    Support Badge component

---
+++ lib/surface_boostrap4/badge.ex
@@ -0,0 +1,40 @@
+defmodule SurfaceBootstrap4.Badge do
+  @moduledoc """
+  Bootstrap4 badge component.
+  """
+
+  use Surface.Component
+
+  @doc "The label of the badge"
+  property label, :string
+
+  @doc "The color of the badge"
+  property color, :string, values: ~w(primary secondary success info warning danger link light dark)
+
+  @doc "Badges more rounded with larger border-radius"
+  property pill, :boolean
+
+  @doc "Triggered on click"
+  property click, :event
+
+  @doc """
+  The content of the generated `<span>` element. If no content is provided,
+  the value of property `label` is used instead.
+  """
+  slot(default)
+
+  def render(assigns) do
+    ~H"""
+    <span
+      :on-phx-click={{@click}}
+      class={{
+        :badge,
+        "badge-pill": @pill,
+        "badge-#{@color}": @color
+      }}
+    >
+      <slot>{{ @label }}</slot>
+    </span>
+    """
+  end
+end
---
+++ test/surface_bootstrap4/badge_test.exs
@@ -0,0 +1,93 @@
+defmodule SurfaceBootstrap4.BadgeTest do
+  use ExUnit.Case, async: true
+  use ExUnit.Parameterized
+
+  alias SurfaceBootstrap4.Badge, warn: false
+  import ComponentTestHelper
+
+  test "creates a <span> with class badge" do
+    expected = """
+    <span class="badge">
+    A badge
+    </span>
+    """
+
+    assert_component(~S(<Badge>A badge</Badge>), expected)
+  end
+
+  test_with_params "property color", &render_and_assert_badge/2 do
+    [
+      primary: {
+        ~S(<Badge color="primary">primary</Badge>),
+        "primary"
+      },
+      secondary: {
+        ~S(<Badge color="secondary">secondary</Badge>),
+        "secondary"
+      },
+      success: {
+        ~S(<Badge color="success">success</Badge>),
+        "success"
+      },
+      info: {
+        ~S(<Badge color="info">info</Badge>),
+        "info"
+      },
+      warning: {
+        ~S(<Badge color="warning">warning</Badge>),
+        "warning"
+      },
+      danger: {
+        ~S(<Badge color="danger">danger</Badge>),
+        "danger"
+      },
+      link: {
+        ~S(<Badge color="link">link</Badge>),
+        "link"
+      },
+      light: {
+        ~S(<Badge color="light">light</Badge>),
+        "light"
+      },
+      dark: {
+        ~S(<Badge color="dark">dark</Badge>),
+        "dark"
+      },
+    ]
+  end
+
+  test_with_params "property pill", &assert_component/2 do
+    [
+      pill: {
+        ~S(<Badge pill>pill</Badge>),
+        """
+        <span class="badge badge-pill">
+        pill
+        </span>
+        """
+      },
+      primary_pill: {
+        ~S(<Badge color="primary" pill>primary pill</Badge>),
+        """
+        <span class="badge badge-pill badge-primary">
+        primary pill
+        </span>
+        """
+      },
+    ]
+  end
+
+  defp render_and_assert_badge(component, expected_color) do
+    expected = """
+    <span class="badge badge-#{expected_color}">
+    #{expected_color}
+    </span>
+    """
+
+    assert render_live(component) =~ expected
+  end
+
+  defp assert_component(component, expected) do
+    assert render_live(component) =~ expected
+  end
+end
