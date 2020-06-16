fde8f24f97c6a5925620f09519ca41093540d921
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-06-16 08:02:26 +0200

    Add phoenix compiler

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SurfaceBootstrap4.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      compilers: [:phoenix] ++ Mix.compilers(),
       deps: deps()
     ]
   end
