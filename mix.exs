4b4ae0979b087714d3fa367b38566d3586fba839
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2021-01-13 22:25:06 +0100

    Update metadata in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,14 +1,20 @@
 defmodule SurfaceBootstrap4.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/tschmidleithner/surface_bootstrap4"
+  @version "0.1.0"
+
   def project do
     [
       app: :surface_bootstrap4,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix] ++ Mix.compilers(),
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: @source_url
     ]
   end
 
@@ -24,6 +30,18 @@ defmodule SurfaceBootstrap4.MixProject do
       {:floki, "~> 0.29", only: :test},
       {:surface, "~> 0.1"},
       {:ex_parameterized, "~> 1.3.7", only: :test}
+    ]
+  end
+
+  defp description do
+    "A set of Surface components based on Bootstrap 4 for Phoenix LiveView."
+  end
+
+  defp package do
+    [
+      maintainers: ["Thomas Schmidleithner"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
